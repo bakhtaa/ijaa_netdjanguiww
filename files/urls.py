3491eda9da55d9009f1fb77dbcb6f5eac7553b6a
Nairouz Bakhta BEN HADJ SLAMA <152972131+bakhtaa@users.noreply.github.com>
2026-02-25 05:21:00 +0000

    facile

--- files/urls.py
+++ files/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from files import views
+#from . import views ( . refers to the current directory)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index, name="index"),
+    path('files/', views.files, name="files")
+
 ]
--- files/views.py
+++ files/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("hello there")
+
+def files(request):
+    # we want to  render our teplate here and send it to the user
+    return render(request,'files/files.html', {'data': 'whats in la la lan'})
